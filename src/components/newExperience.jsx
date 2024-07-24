1e177d1f7ca6a5196480502ed066c7848637097c
syhrezz <exzpect@gmail.com>
2024-07-24 18:05:41 +0700

    update hover behavior at experiences page

--- src/components/newExperience.jsx
+++ src/components/newExperience.jsx
@@ -107,7 +107,7 @@ const NewExperience = () => {
               exit={{ opacity: 0, y: 20 }}
               transition={{ duration: 0.3 }}
             >
-              Hover effect active for {experiences[hoveredIndex].company}
+              {experiences[hoveredIndex].role} at {experiences[hoveredIndex].company}
             </motion.div>
           )}
         </AnimatePresence>
